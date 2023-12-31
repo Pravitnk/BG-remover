b95ab2f03c5127be2b7871c066a1d84685607ad0
PRAVIT <pravitnaik656@gmail.com>
2024-01-01 00:00:05 +0530

    server edited

--- sever/routes/userRoute.js
+++ sever/routes/userRoute.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { clerkWebhooks, userCredits } from "../controller/userController.js";
 import authUser from "../middlewares/auth.js";
+import bodyParser from "body-parser";
 
 const userRouter = express.Router();
 
-userRouter.post("/webhooks", clerkWebhooks);
+// userRouter.post("/webhooks", clerkWebhooks);
+userRouter.post(
+  "/api/user/webhooks",
+  bodyParser.raw({ type: "application/json" }),
+  clerkWebhooks
+);
+
 userRouter.get("/credits", authUser, userCredits);
 
 export default userRouter;
