61b1342d2992ab23982e6ae739c850ec20c7ba2d
PRAVIT <pravitnaik656@gmail.com>
2024-10-21 17:07:47 +0530

    "Updated user controller "

--- sever/controller/user_controller.js
+++ sever/controller/user_controller.js
@@ -4,6 +4,8 @@ import { Webhook } from "svix";
 //api controller function to manage Clerk user with database
 //http://localhost:4000/api/user/webhooks
 const clerkWebhooks = async (req, res) => {
+  console.log("11");
+
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
     await whook.verify(JSON.stringify(req.body), {
@@ -12,8 +14,10 @@ const clerkWebhooks = async (req, res) => {
       "svix-signature": req.headers["svix-signature"],
     });
     const { data, type } = req.body;
+    console.log("Webhook Body:", data);
+
     switch (type) {
-      case "user-created": {
+      case "user.created": {
         const userData = {
           clerkId: data.id,
           email: data.email_addresses[0].email_address,
@@ -25,7 +29,7 @@ const clerkWebhooks = async (req, res) => {
         res.json({});
         break;
       }
-      case "user-updated": {
+      case "user.updated": {
         const userData = {
           email: data.email_addresses[0].email_address,
           photo: data.image_url,
@@ -36,7 +40,7 @@ const clerkWebhooks = async (req, res) => {
         res.json({});
         break;
       }
-      case "user-deleted": {
+      case "user.deleted": {
         await userModel.findOneAndDelete({ clerkId: data.id });
         res.json({});
         break;
@@ -53,4 +57,22 @@ const clerkWebhooks = async (req, res) => {
   }
 };
 
-export { clerkWebhooks };
+// api controller function to get user available credits data
+
+const userCredits = async (req, res) => {
+  try {
+    const { clerkId } = req.body;
+    const userData = await userModel.findById({ clerkId });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    res.json({ success: true, credits: userData.creditBalance });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export { clerkWebhooks, userCredits };
--- sever/routes/user_route.js
+++ sever/routes/user_route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { clerkWebhooks } from "../controller/user_controller.js";
+import { clerkWebhooks, userCredits } from "../controller/user_controller.js";
+import authUser from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/webhooks", clerkWebhooks);
+userRouter.get("/credits", authUser, userCredits);
 
 export default userRouter;
