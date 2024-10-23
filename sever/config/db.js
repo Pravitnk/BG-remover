62ac4f490502baf07f0f1f6c904a90a51cfc74ea
PRAVIT <pravitnaik656@gmail.com>
2024-10-23 13:15:31 +0530

    new

--- sever/config/db.js
+++ sever/config/db.js
@@ -4,14 +4,10 @@ const connectDb = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Successfully connected to MongoDB");
   });
-  await mongoose.connect(
-    `${process.env.MONGO_URI}/
-Bg-removal`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  await mongoose.connect(`${process.env.MONGO_URI}/Bg-removal`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 export default connectDb;
